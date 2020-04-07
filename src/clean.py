ea7215e3b6984e1515757b8e8943208e5203742d
boh016 <38054630+boh016@users.noreply.github.com>
2020-04-06 21:23:11 -0700

    adding cleaning functions to extract DUI data

--- src/clean.py
+++ src/clean.py
@@ -16,6 +16,14 @@ def old_dui(old_stop):
     (copy.search_details_description.str.contains(DUI_lst[1]))|\
     (copy.search_details_description.str.contains(DUI_lst[2]))]
 
-
+# return subset of the new stop data with only DUI related record
 def new_dui(new_stop):
-    
+    copy = new_stop.copy(deep=True)
+    copy.reason_for_stop_explanation = copy.reason_for_stop_explanation.str.upper()
+    return copy[copy.reason_for_stop_explanation.str.contains("DUI")]
+
+###############################################################################
+#-------------------------collision data--------------------------------------------
+# return subset of the collision data with only DUI related record
+def collision_dui(collision_df):
+    return collision_df[collision_df.charge_desc.str.contains("DUI")]
